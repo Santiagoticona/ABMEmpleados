fd17508c634adbae6097963902e444f08d1190f9
Santiago <santiago.ticona@hotmail.com>
2024-06-05 22:40:12 -0300

    Se agrega el Proyecto

---
+++ ABMEmpleados/Models/AbmContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ABMEmpleados.Models;
+
+public partial class AbmContext : DbContext
+{
+    public AbmContext()
+    {
+    }
+
+    public AbmContext(DbContextOptions<AbmContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            //      => optionsBuilder.UseSqlServer("server=localhost; database=ABM; integrated security=true; TrustServerCertificate=Yes");
+        }
+    
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__EMPLEADO__3214EC0744E9CE8B");
+
+            entity.ToTable("EMPLEADOS");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Fecha).HasColumnType("date");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ABMEmpleados/Models/Empleado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABMEmpleados.Models;
+
+public partial class Empleado
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public DateTime? Fecha { get; set; }
+
+    public string? Clave { get; set; }
+}
